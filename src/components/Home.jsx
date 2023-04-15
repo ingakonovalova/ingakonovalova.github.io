6733c3539075ccb512b5cce6421baa253e9379f3
Inga Konovalova <inga.konovalova@pipedrive.com>
2023-04-16 01:27:22 +0300

    fix button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,12 +26,14 @@ const Home = () => {
           including versions of Lorem Ipsum.
         </p>
         <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Experience
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
+          <Link to="experience" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View Experience
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
