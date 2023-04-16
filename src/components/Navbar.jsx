e7a72cd8e8428d2439258cab8c449b42f4487830
Inga Konovalova <inga.konovalova@pipedrive.com>
2023-04-16 19:21:36 +0300

    change logo width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "70px" }} />
+        <img src={Logo} alt="Logo Image" style={{ width: "90px" }} />
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
